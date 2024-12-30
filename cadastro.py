3dc0ceeb887a07d560ffc6811d6fbb0de12d5e4f
carlosjr1227 <128106044+carlosjr1227@users.noreply.github.com>
2024-12-29 23:12:37 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome: \n")
+altura = float(input("Digite sua altura: \n"))
+idade = int(input("Digite sua idade: \n"))
+
+# Considerando o ano 2024 como ano atual
+
+ano = 2024- idade
+
+print("O seu nome é:",nome)
+print("A sua altura é",altura)
+print("A sua idade é", idade)
+print("O seu ano de nascimento é",ano)
